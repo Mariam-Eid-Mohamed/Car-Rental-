d24f0ef15ddc3364c3682e4b41f1e38ebfb248b2
mariameid <mariameid210@gmail.com>
2025-08-06 00:09:41 +0200

    added notfound component

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+import NotfoundImg from "../../assets/images/not-found/404-not-found.png";
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <>
+      <div className="d-flex vh-100 bg-primary  position-relative">
+        <img
+          src={NotfoundImg}
+          alt=""
+          className=" w-100 h-100 object-fit-contain "
+        />
+      </div>
+    </>
+  );
 }
 
 export default NotFound;
